48081bca404c2f2b423863b19fb0bc8d1605026e
omarmeotaz-work <omarmoetaz1997@gmail.com>
2025-02-23 14:27:24 +0200

    edit added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 const list = document.getElementById("list");
 const add = document.getElementById("add-button");
 const itemfield = document.getElementById("input-field");
-
 //add task by click
 add.addEventListener("click", function () {
   if (itemfield.value != "") {
     const newItem = document.createElement("li");
     const newbutton = document.createElement("button");
+    const newEdit = document.createElement("button");
+    newEdit.className = "edit-btn";
+    newbutton.className = "del-btn";
     newItem.textContent = itemfield.value;
     newbutton.textContent = "x";
+    newEdit.textContent = "Edit";
+    newItem.appendChild(newEdit);
     newItem.appendChild(newbutton);
     list.appendChild(newItem);
+    edit();
     remove();
     finished();
     itemfield.value = "";
@@ -22,18 +27,24 @@ itemfield.addEventListener("keydown", function (input) {
   if (input.key === "Enter" && itemfield.value != "") {
     const newItem = document.createElement("li");
     const newbutton = document.createElement("button");
+    const newEdit = document.createElement("button");
+    newEdit.className = "edit-btn";
+    newbutton.className = "del-btn";
     newItem.textContent = itemfield.value;
     newbutton.textContent = "x";
-    list.appendChild(newItem);
+    newEdit.textContent = "Edit";
+    newItem.appendChild(newEdit);
     newItem.appendChild(newbutton);
+    list.appendChild(newItem);
     remove();
     finished();
+    edit();
     itemfield.value = "";
   }
 });
 //function to close task
 function remove() {
-  const button = document.querySelectorAll("button");
+  const button = document.querySelectorAll(".del-btn");
   button.forEach(function (input) {
     input.addEventListener("click", function (e) {
       e.stopPropagation();
@@ -52,6 +63,23 @@ function finished() {
     });
   });
 }
+//function to edit
+function edit() {
+  const edit = document.querySelectorAll(".edit-btn");
+  edit.forEach(function (input) {
+    input.addEventListener("click", function (e) {
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      const pr = prompt("edit your task");
+      console.log(edit);
+      if (pr != "") {
+        const newT = input.parentElement;
+        newT.firstChild.textContent = pr;
+      } else alert("empty input is invalid");
+    });
+  });
+}
 
 remove();
 finished();
+edit();
